c03ba7609c774ed7b16a151cc4be086f7a365bf3
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-05 17:34:31 +0300

    fix mobx

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ export default {
   presets: [],
   plugins: [
     ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
     "babel-plugin-transform-typescript-metadata",
     "babel-plugin-parameter-decorator",
   ],
