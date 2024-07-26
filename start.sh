88700e8b67575a05214ceb8789c2bd365ddaa6ab
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-27 00:41:34 +0300

    refactoring

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t wireguard-admin:latest .
-[[ $(docker ps -f name=wireguard-admin_container -q -a) != '' ]] && docker rm --force $(docker ps -f name=wireguard-admin_container -q -a)
-docker run -u root -d --restart=always --network server-net -p 8181:4173 --name wireguard-admin_container wireguard-admin:latest
+docker build -f Dockerfile -t wireguard-web:latest .
+[[ $(docker ps -f name=wireguard-web_container -q -a) != '' ]] && docker rm --force $(docker ps -f name=wireguard-web_container -q -a)
+docker run -u root -d --restart=always --network server-net -p 8181:4173 --name wireguard-web_container wireguard-web:latest
 docker image prune -a --force
