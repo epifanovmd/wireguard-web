271fc776bde5ca13dfda9dc18a67fa4f4a2aa4be
Андрей Епифанов <epifanovmd@gmail.com>
2024-12-05 14:31:18 +0300

    refactoring

--- src/components/forms/client/validation.ts
+++ src/components/forms/client/validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const clientValidation = z.object({
   name: z
     .string()
-    .min(3, { message: "Минимальная длинна названия клиента 3 символа" }),
+    .min(3, { message: "Минимальная длинна имени клиента 3 символа" }),
   enabled: z.boolean().optional(),
 });
 
