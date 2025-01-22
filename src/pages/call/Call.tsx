1b5966ef4b24340e9957ee204a9213d646988b75
Андрей Епифанов <epifanovmd@gmail.com>
2025-01-23 00:36:36 +0300

    refactoring call

--- src/pages/call/Call.tsx
+++ src/pages/call/Call.tsx
@@ -44,7 +44,7 @@ export const CallPage: FC<PropsWithChildren<IProps>> = observer(() => {
   return (
     <div className={"flex flex-col"}>
       <div className="flex justify-center items-center bg-gray-100 m-auto">
-        <div className="lg:flex shadow-md rounded-lg overflow-hidden h-[600px] w-[300px] lg:h-[300px] lg:w-[600px]">
+        <div className="lg:flex shadow-md rounded-lg overflow-hidden h-[500px] w-[300px] lg:h-[300px] lg:w-[500px]">
           <video
             className="lg:w-[50%] lg:h-[100%] w-[100%] h-[50%] scale-x-[-1] object-cover bg-gray-300 rounded-lg"
             ref={localStreamRef}
